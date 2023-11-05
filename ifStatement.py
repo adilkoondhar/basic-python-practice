3b83baa041664576d474e667398dbc8c1d09846b
Adil Koondhar <146661723+adilkoondhar@users.noreply.github.com>
2023-11-05 14:58:37 +0500

    Add if statement file

---
+++ ifStatement.py
@@ -0,0 +1,9 @@
+ch = input ('Enter a character: ')
+if ch == 'A' or ch == 'a':
+    print ("It's either A or a")
+elif ch == 'E' or ch == 'e':
+    print ("It's either E or e")
+elif ch == 'S' or ch == 's':
+    print ("It's either S or s")
+else:
+    print ('It\'s a character')
