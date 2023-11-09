b2a0d7aa9b7a33c89b1310289cd3bdebd9434a87
Adil Koondhar <146661723+adilkoondhar@users.noreply.github.com>
2023-11-09 14:17:03 +0500

    Add string format and functions files

---
+++ strFormatting.py
@@ -0,0 +1,5 @@
+date = [1, 7, 22]
+strDate = "Date: {0}/{1}/{2}".format(date[0], date[1], date[2])
+print (strDate)
+strDate2 = "Date: {x}/{y}".format(x = 1, y = 7)
+print (strDate2)
---
+++ strFunctions.py
@@ -0,0 +1,16 @@
+print (",".join(["Apple", "Banana", "Mango"]))
+list = ["Apple", "Banana", "Mango"]
+print (":".join(list))
+
+print ("Hey there".replace("there", "world"))
+a = "Hey bro"
+print (a.replace("bro", "there"))
+
+b = "This is a string"
+print (b.startswith("This"))
+print (b.endswith("This"))
+
+print (b.upper())
+print (b.lower())
+
+print (b.capitalize())
