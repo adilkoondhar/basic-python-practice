76f747c26c8f8f7269f61a29cb99fccefb8047d1
Adil Koondhar <146661723+adilkoondhar@users.noreply.github.com>
2023-11-14 16:18:14 +0500

    Add sets file

---
+++ sets.py
@@ -0,0 +1,17 @@
+numSet = {1, 2, 3}
+
+print(numSet)
+
+print(3 in numSet)
+
+numSet.add(4)
+numSet.remove(1)
+
+print(numSet)
+
+setA = {1, 2, 3}
+setB = {3, 4, 5}
+
+print(setA | setB) #Set union
+print(setA & setB) #Set intersection
+print(setA - setB) #Set compliment
